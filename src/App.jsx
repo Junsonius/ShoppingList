a060a8c6bca6fa358ffc9a631e9f4914ef996f6d
Junsonius <sandrotsjunior+github@gmail.com>
2024-05-08 17:04:36 -0300

    added styling to footer and cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from "./components/Header"
@@ -6,18 +7,31 @@ import ShoppingList from "./components/ShoppingList"
 import './App.css'
 import PriceFooter from './components/PriceFooter';
 
-import { useState, createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 
 import { Container, Button, Collapse, Modal, ModalBody} from 'react-bootstrap';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const listItemContext = createContext()
+export const windowWidth = createContext()
 
 function App() {
 
-const [listItems, setListItem] = useState([{product: 'Bananananan', qnt: "1", unit: "kg", price: 12.00 , checked: false, id: Date.now()}, {product: 'potatooo', qnt: "1", unit: "kg", price: 6.00 , checked: false, id: (Date.now()+1)}])
+const [listItems, setListItem] = useState([{product: 'Bananananan', qnt: "1", unit: "kg", price: 12.00 , checked: false, id: Date.now()}, {product: 'potatooo', qnt: "1", unit: "kg", price: 6.00 , checked: false, id: (Date.now()+1)}, {product: 'Bananananan', qnt: "1", unit: "kg", price: 12.00 , checked: false, id: Date.now()+2}, {product: 'potatooo', qnt: "1", unit: "kg", price: 6.00 , checked: false, id: (Date.now()+3)}])
 const [activeForm, setActiveForm] = useState(false)
 const [activeModal, setActiveModal] = useState(false)
+const [width, setWidth] = useState(window.innerWidth)
+
+useEffect(() => {
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+  window.addEventListener('resize', handleResize);
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
+
 
 function openForm() {
   setActiveForm(!activeForm)
@@ -34,7 +48,9 @@ const handleClose = () => setActiveModal(false);
 const handleShow = () => setActiveModal(true);
 
   return (
-    <Container className='min-vh-100 p-3'>
+    <windowWidth.Provider value={width}>
+
+    <Container className='min-vh-100 p-3 mb-5'>
       <Header />
       
       <Container className='d-flex flex-column gap-3 mt-5 h-50'>
@@ -47,7 +63,7 @@ const handleShow = () => setActiveModal(true);
           </Collapse>
         {
           listItems.length === 0 ? '' : <> <ShoppingList  openForm={openForm} />
-          <Button className='w-25 align-self-end' onClick={handleShow} variant='danger'>Limpar Lista</Button>
+          <Button className='w-25 align-self-end mb-2' onClick={handleShow} variant='danger'>Limpar Lista</Button>
           <PriceFooter />
         </>}
       </listItemContext.Provider>
@@ -61,6 +77,8 @@ const handleShow = () => setActiveModal(true);
         </ModalBody>
       </Modal>
     </Container>
+    
+    </windowWidth.Provider>
 
 
   )
--- src/components/PriceFooter.jsx
+++ src/components/PriceFooter.jsx
@@ -1,31 +1,61 @@
 import { useContext, useEffect, useState } from "react"
-import { listItemContext } from "../App"
-
+import { listItemContext, windowWidth } from "../App"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faCalculator } from "@fortawesome/free-solid-svg-icons"
 
 function PriceFooter() {
 
     const {listItems} = useContext(listItemContext)
-
     const [totalPrice, setTotalPrice] = useState(0)
+    const [cart, setCart] = useState(0)
+    const [shaking, setShaking] = useState(false)
+
+    const width = useContext(windowWidth)
+
+  function shakeTimer() {
+    setTimeout(() => {
+      setShaking(false)
+    }, 800);
+  }
 
     useEffect(() => {
         if (listItems.length === 0) return setTotalPrice(0)
+        
         const itemPrices = listItems.map((item) => {
             return item.price * item.qnt
           })
 
         setTotalPrice(parseFloat(itemPrices.reduce(function(acc, cur) {return acc + cur})).toFixed(2))
-      }, [listItems])
+        
+        function updateCart() {
 
+          const cartAmount = listItems.filter(item => item.checked)
+          if (cartAmount.length === 0) return setCart(0)
+          
+          const cartSum = cartAmount.map((item) => {
+            return item.price * item.qnt
 
-      useEffect(() => {
+          })
+          setCart(cartSum.reduce(function(acc, cur) {return acc + cur}).toFixed(2))
+          setShaking(true)
+          shakeTimer()
+        }
 
-      })
+        updateCart()
+
+      }  , [listItems])
 
     return (
-        <div>
+        <div className={`d-flex ${width < 576 ? "fixed-bottom rounded-top" : "sticky-bottom rounded" } justify-content-around p-3 bg-success`}>
             
-            <h2>Sua compra terá um valor total de R$ {totalPrice} reais</h2>
+            <span className="d-flex gap-2 align-items-center">
+            <FontAwesomeIcon icon={faCalculator} size="lg" color="white" />
+              <p>Total R$ {totalPrice}</p>
+            </span>
+            <span className="d-flex gap-2 align-items-center">
+              <FontAwesomeIcon icon={faCartShopping} size="lg"color="white" shake={shaking ? true : false} />
+              <p>Carrinho R$ {parseFloat(cart).toFixed(2)}</p>
+            </span>
         </div>
     )
 }
